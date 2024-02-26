33ae1e100c037aba937438bce149d38ddf57d578
Vini5577 <ugopessoa@gmail.com>
2024-02-26 10:47:21 -0300

    finished loading screen

--- screens/MovieScreen.tsx
+++ screens/MovieScreen.tsx
@@ -9,6 +9,7 @@ import { LinearGradient } from 'expo-linear-gradient'
 
 import { MovieList } from '../components/movieList'
 import { Cast } from '../components/cast'
+import Loading from '../components/loading'
 
 var { width, height } = Dimensions.get('window');
 const ios = Platform.OS == 'ios';
@@ -20,6 +21,7 @@ export const MovieScreen = () => {
   const navigation = useNavigation();
   const [cast, setCast] = useState([1, 2, 3, 4, 5]);
   const [similarMovies, setSimilarMovies] = useState([1, 2, 3, 4, 5]);
+  const [loading, setLoading] = useState(false);
 
   let movieName = 'Spider-Man No Way Home'
 
@@ -42,18 +44,25 @@ export const MovieScreen = () => {
             <HeartIcon size="35" color={isFavourite ? themeColors.background : "white"} />
           </TouchableOpacity >
         </SafeAreaView>
-        <View>
-          <Image
-            source={require('../assets/poster/poster-1.jpg')}
-            style={{ width: width, height: height * 0.55 }} />
-          <LinearGradient
-            colors={['transparent', 'rgba(23,23,23, 0.8)', 'rgba(23,23,23, 1)']}
-            style={{ width, height: height * 0.40 }}
-            start={{ x: 0.5, y: 0 }}
-            end={{ x: 0.5, y: 1 }}
-            className='absolute bottom-0'
-          />
-        </View>
+        {
+          loading ? (
+            <Loading />
+          ) : (
+            <View>
+              <Image
+                source={require('../assets/poster/poster-1.jpg')}
+                style={{ width: width, height: height * 0.55 }} />
+              <LinearGradient
+                colors={['transparent', 'rgba(23,23,23, 0.8)', 'rgba(23,23,23, 1)']}
+                style={{ width, height: height * 0.40 }}
+                start={{ x: 0.5, y: 0 }}
+                end={{ x: 0.5, y: 1 }}
+                className='absolute bottom-0'
+              />
+            </View>
+          )
+        }
+
       </View>
 
       {/* movie details */}
--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import { Dimensions, Image, ScrollView, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import { XMarkIcon } from 'react-native-heroicons/outline';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Loading from '../components/loading';
 
 const { width, height } = Dimensions.get('window')
 
 export const SearchScreen: React.FC = () => {
     const navigation = useNavigation();
     const [results, setResults] = useState([1, 2, 3, 4]);
+    const [loading, setLoading] = useState(false);
     let movieName = 'Spider-Man: No Way Home'
 
     return (
@@ -29,46 +31,51 @@ export const SearchScreen: React.FC = () => {
             </View>
             {/* results */}
             {
-                results.length > 0 ? (
-                    <ScrollView
-                        showsVerticalScrollIndicator={false}
-                        contentContainerStyle={{ paddingHorizontal: 15 }}
-                        className='space-y-3'
-                    >
-                        <Text className='text-white font-semibold ml-1'>Results ({results.length})</Text>
-                        <View className='flex-row justify-between flex-wrap'>
-                            {
-                                results.map((item: any, index: any) => {
-                                    return (
-                                        <TouchableWithoutFeedback
-                                            key={index}
-                                            onPress={() => navigation.push("Movie", item)}
-                                        >
-                                            <View className='space-y-2 mb-4'>
-                                                <Image className='rounded-3xl'
-                                                    source={require('../assets/poster/poster-1.jpg')}
-                                                    style={{ width: width * 0.44, height: height * 0.3 }}
-                                                />
-                                                <Text className='text-neutral-400 ml-1'>
-                                                    {
-                                                        movieName.length > 22 ? movieName.slice(0, 22) + '...' : movieName
-                                                    }
-                                                </Text>
-                                            </View>
-                                        </TouchableWithoutFeedback>
-                                    )
-                                })
-                            }
-                        </View>
-                    </ScrollView>
+                loading ? (
+                    <Loading />
                 ) : (
-                    <View className='flex-row justify-center'>
-                        <Image source={require('../assets/time/movie.png')}
-                            className='h-96 w-96'
-                        />
-                    </View>
+                    results.length > 0 ? (
+                        <ScrollView
+                            showsVerticalScrollIndicator={false}
+                            contentContainerStyle={{ paddingHorizontal: 15 }}
+                            className='space-y-3'
+                        >
+                            <Text className='text-white font-semibold ml-1'>Results ({results.length})</Text>
+                            <View className='flex-row justify-between flex-wrap'>
+                                {
+                                    results.map((item: any, index: any) => {
+                                        return (
+                                            <TouchableWithoutFeedback
+                                                key={index}
+                                                onPress={() => navigation.push("Movie", item)}
+                                            >
+                                                <View className='space-y-2 mb-4'>
+                                                    <Image className='rounded-3xl'
+                                                        source={require('../assets/poster/poster-1.jpg')}
+                                                        style={{ width: width * 0.44, height: height * 0.3 }}
+                                                    />
+                                                    <Text className='text-neutral-400 ml-1'>
+                                                        {
+                                                            movieName.length > 22 ? movieName.slice(0, 22) + '...' : movieName
+                                                        }
+                                                    </Text>
+                                                </View>
+                                            </TouchableWithoutFeedback>
+                                        )
+                                    })
+                                }
+                            </View>
+                        </ScrollView>
+                    ) : (
+                        <View className='flex-row justify-center'>
+                            <Image source={require('../assets/time/movie.png')}
+                                className='h-96 w-96'
+                            />
+                        </View>
+                    )
                 )
             }
+
 
         </SafeAreaView>
     );
